054d641188a2ba2765a2cc97e9163d8fd42de12b
tanawatmix <s6552410002@sau.ac.th>
2024-11-12 14:07:16 +0700

    2 Commit

--- test_app.py
+++ test_app.py
@@ -2,6 +2,6 @@ import pytest
 from app import add
 
 def test_add():
-    assert add(1, 2) == 3
+    assert add(2, 3) == 5
     assert add(-1, 1) == 0
-    assert add(10, 5) == 15
+    assert add(5, 10) == 20
